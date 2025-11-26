7b9f03751b5af70e7397eb166df8bf3ec1e76341
Daniel <danielsnadon@gmail.com>
2025-11-26 20:05:39 +0300

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/snadon/OOP-Lab-6
-# Build directory: /home/snadon/OOP-Lab-6/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
